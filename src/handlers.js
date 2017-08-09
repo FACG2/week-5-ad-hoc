2383137cd6bba7e3ee6ce3869f850d75a40dffc4
eslam3bed <iah-93@hotmail.com>
2017-08-09 22:00:37 +0300

    refactoring

--- src/handlers.js
+++ src/handlers.js
@@ -1,66 +1,84 @@
-const fs=require('fs');
-const request=require('./request')
-const path=require('path')
+const fs = require('fs');
+const request = require('./request')
+const path = require('path')
 
-const home=(req ,res)=>{
-  const ext =  req.url.split('.')[1];
-  fs.readFile(path.join(__dirname, '..', 'public', 'index.html') , (error, data) => {
-    if(error){
-      res.writeHead(404, {'content-type' : 'text/html'});
+const home = (req, res) => {
+  const ext = req.url.split('.')[1];
+  fs.readFile(path.join(__dirname, '..', 'public', 'index.html'), (error, data) => {
+    if (error) {
+      res.writeHead(404, {
+        'content-type': 'text/html'
+      });
       res.end('Page Not Found')
-    }else {
-      res.writeHead(200, {'content-type': 'text' + ext});
+    } else {
+      res.writeHead(200, {
+        'content-type': 'text' + ext
+      });
       res.end(data);
     }
   });
 }
-const Public=(req ,res)=>{
+const Public = (req, res) => {
 
-  let ext =  req.url.split('.')[1];
+  let ext = req.url.split('.')[1];
   // let file= req.url.substring(1,req.url.length);
-  fs.readFile(path.join(__dirname, '..', req.url) , (error, data) => {
-    if(error){
-      res.writeHead(404, {'content-type' : 'text/plain'});
+  fs.readFile(path.join(__dirname, '..', req.url), (error, data) => {
+    if (error) {
+      res.writeHead(404, {
+        'content-type': 'text/plain'
+      });
       res.end('Page Not Found')
-    }else {
-      res.writeHead(200, {'content-type': 'text/' + ext});
+    } else {
+      res.writeHead(200, {
+        'content-type': 'text/' + ext
+      });
       res.end(data);
     }
   });
 }
-const getQoute=(req ,res)=>{
-  request.getQoute((err , response , body )=>{
-    if(err){
-      res.writeHead(404, {'content-type' : 'text/html'});
+const getQoute = (req, res) => {
+  request.getQoute((err, response, body) => {
+    if (err) {
+      res.writeHead(404, {
+        'content-type': 'text/html'
+      });
       res.end('Page Not Found')
-    }else{
-      res.writeHead(200, {'content-type': 'application/json'});
+    } else {
+      res.writeHead(200, {
+        'content-type': 'application/json'
+      });
       res.end(body);
     }
   })
 }
-const getAudio=(req ,res)=>{
-  let txt=decodeURIComponent(req.url.split('=')[1])
-  request.getAudio(txt ,(err , response , body )=>{
-    if(err){
-      res.writeHead(404, {'content-type' : 'text/html'});
+const getAudio = (req, res) => {
+  let txt = decodeURIComponent(req.url.split('=')[1])
+  request.getAudio(txt, (err, response, body) => {
+    if (err) {
+      res.writeHead(404, {
+        'content-type': 'text/html'
+      });
       res.end('Page Not Found')
-    }else{
-      res.writeHead(200, {'content-type': 'application/json'});
+    } else {
+      res.writeHead(200, {
+        'content-type': 'application/json'
+      });
       res.end(body);
     }
   })
 }
-const notFound=(req ,res)=>{
-  res.writeHead(404, {'content-type' : 'text/html'});
+const notFound = (req, res) => {
+  res.writeHead(404, {
+    'content-type': 'text/html'
+  });
   res.end('Page Not Found')
 }
 
 
-module.exports={
-  home:home,
-  Public:Public,
-  getQoute:getQoute,
-  getAudio:getAudio,
-  notFound:notFound
+module.exports = {
+  home: home,
+  Public: Public,
+  getQoute: getQoute,
+  getAudio: getAudio,
+  notFound: notFound
 }
